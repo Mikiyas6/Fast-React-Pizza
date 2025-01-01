997ff61055d95a66d2460531977a96d751fcf19b
Mikiyas6 <Mikitedi9@gmail.com>
2025-01-01 18:32:30 +0300

    Displaying a loading indicator

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -1,9 +1,19 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 function AppLayout() {
+  const navigation = useNavigation();
+  // It provides information about the current navigation state of the application
+  //   The navigation object contains the state of the navigation process, with key properties like:
+  // state: Indicates the current state of the navigation. Common values include:
+  // "idle": No navigation is occurring.
+  // "loading": A route transition is in progress (e.g., fetching data or loading a new route).
+  // "submitting": A form submission is happening during navigation.
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -3,6 +3,7 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
+
   return (
     <ul>
       {menu.map((pizza) => (
