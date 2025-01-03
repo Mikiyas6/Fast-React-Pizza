83ad3d51e145743c9ad2f468bd556f494e29c19b
Mikiyas6 <Mikitedi9@gmail.com>
2025-01-03 14:29:25 +0300

    Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Mike</div>;
+  return <div className="hidden text-sm font-semibold md:block">Mike</div>;
 }
 
 export default Username;
