67d617f55a31017144ef26e9b659762c7825b3a1
Mikiyas6 <Mikitedi9@gmail.com>
2025-01-08 14:34:18 +0300

    Using Cart for New Orders

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,52 +1,37 @@
 import { useState } from "react";
 import Button from "../../UI/Button";
-import {
-  Form,
-  redirect,
-  useActionData,
-  useNavigate,
-  useNavigation,
-} from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import { useSelector } from "react-redux";
-
+import {
+  clearCart,
+  getCart,
+  getTotalCartPrice,
+  getUsername,
+} from "../cart/cartSlice";
+import EmptyCart from "../cart/EmptyCart";
+import store from "../../store";
+import { formatCurrency } from "../../utils/helpers";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
+const PRIORITY_RATE = 0.2;
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
+  const cart = useSelector(getCart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
-  const username = useSelector((store) => store.user.username);
+  const username = useSelector(getUsername);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? PRIORITY_RATE * totalCartPrice : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+  if (!cart.length) return <EmptyCart />;
+  function handlePriority() {
+    setWithPriority((withPriority) => !withPriority);
+  }
   return (
     <div className="space-y-6">
       <h2>Ready to order? Lets go! </h2>
@@ -84,16 +69,19 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={handlePriority}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+
           <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting ? "Placing Order" : "Order Now"}
+            {isSubmitting
+              ? "Placing Order...."
+              : `Order Now For ${formatCurrency(totalPrice)}  `}
           </Button>
         </div>
       </Form>
@@ -104,17 +92,20 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  console.log(data);
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
+  console.log(order);
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
       "Please give us your correct phone number. We might need to contact you.";
   if (Object.keys(errors).length > 0) return errors;
   const newOrder = await createOrder(order);
+  store.dispatch(clearCart());
   return redirect(`/order/${newOrder.id}`);
 }
 
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -21,6 +21,7 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
+  console.log(priority);
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
   return (
     <div className="space-y-8 px-4 py-6">
@@ -52,7 +53,7 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
